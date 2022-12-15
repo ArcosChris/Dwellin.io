d90e03a0819e23825ddd5c42095a9cb5b9f927fc
Christopher Arcos <arcoschris456@gmail.com>
2022-12-15 14:06:22 -0800

    changes hello world

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Friends!!</h1>
       <Button variant="outline-secondary" className='btn btn-outline'>TEST</Button>
 
     </div>
