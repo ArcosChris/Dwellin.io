cdc023d4cc950a2a0af595cbe9e67e94e8f441a3
wrijlarr <wrijlarr@amazon.com>
2022-12-20 15:31:20 -0500

    new changes

--- src/pages/ServiceRequest.js
+++ src/pages/ServiceRequest.js
@@ -1,21 +1,55 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import "../css/ServiceRequest.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const ServiceRequest = () => {
     return (
-        <div className="srp">
-            <h1 className="service">Service Request</h1>
+        <div className="srcontainer">
+            <h1 className="service text-center">Service Request
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh2QVklNtRaXvWoiVDCE27akhKfTkhrCk0Mw&usqp=CAU" alt="" /> </h1>
 
-            <Card className="desc" style={{ width: '30rem' }}>
-                <Card.Body>
-                    <Card.Title>Description of Problem</Card.Title>
-                    <br />
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Form>
+                <InputGroup>
+                    <InputGroup.Text>Date of Issue:</InputGroup.Text>
+                    <Form.Control type="date" aria-label="With textarea" />
+                </InputGroup>
+                <br />
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label><h5>Description of Problem</h5></Form.Label>
+                    <InputGroup>
+                        <InputGroup.Text>Issue</InputGroup.Text>
+                        <Form.Control as="textarea" aria-label="With textarea" />
+                    </InputGroup>
+                    <Form.Text className="text-muted">
+                        Give a detiled description of the issue.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label><h5>Area Affected</h5></Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option></option>
+                        <option value="1">Appliances</option>
+                        <option value="2">Electrical</option>
+                        <option value="3">AC</option>
+                        <option value="4">Plumbing</option>
+                        <option value="3">Structural</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <p>Importance:</p>
+                    <Form.Check type="checkbox" label="High Priority" />
+                    <Form.Check type="checkbox" label="Med Priority" />
+                    <Form.Check type="checkbox" label="Low Priority" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
         </div>
     );
 }
